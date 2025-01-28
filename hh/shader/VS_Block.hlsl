ce35ab1958fe14ae9cad363e57743507360f7aa9
TKG-master <kuozakisaika336@gmail.com>
2025-01-28 20:04:56 +0900

    使ってないシェーダーコメント化

--- hh/shader/VS_Block.hlsl
+++ hh/shader/VS_Block.hlsl
@@ -1,32 +1,32 @@
 #include "common.hlsl"
 
-PS_IN main(VS_IN vin)
-{
-    PS_IN vout;
+//PS_IN main(VS_IN vin)
+//{
+// //   PS_IN vout;
 	
-    float4 Pos = float4(vin.Position.xyz,1.0f);
-	//vout.pos = vin.pos;
-    vout.pos = mul(vin.Position, World);
-	// ピクセルシェーダーに渡すために、途中で変数にコピー
-    //vout.worldPos.x =World._m30_m33_m33_m33 ;
-    //vout.worldPos.y =World._m31_m33_m33_m33 ;
-    //vout.worldPos.z = World._m32_m33_m33_m33;
-    vout.worldPos = vout.pos;
+// //   float4 Pos = float4(vin.Position.xyz,1.0f);
+//	////vout.pos = vin.pos;
+// //   vout.pos = mul(vin.Position, World);
+//	//// ピクセルシェーダーに渡すために、途中で変数にコピー
+// //   //vout.worldPos.x =World._m30_m33_m33_m33 ;
+// //   //vout.worldPos.y =World._m31_m33_m33_m33 ;
+// //   //vout.worldPos.z = World._m32_m33_m33_m33;
+// //   vout.worldPos = vout.pos;
     
-    matrix lwvp;
-    lwvp = mul(World, LightView);
-    lwvp = mul(lwvp, LightProjection);
+// //   matrix lwvp;
+// //   lwvp = mul(World, LightView);
+// //   lwvp = mul(lwvp, LightProjection);
    
-    vout.ShadowPos = mul(Pos, lwvp);
+// //   vout.ShadowPos = mul(Pos, lwvp);
 
     
-    vout.pos = mul(vout.pos, View);
-    vout.pos = mul(vout.pos, Projection);
+// //   vout.pos = mul(vout.pos, View);
+// //   vout.pos = mul(vout.pos, Projection);
 
-    vout.screenPos = vout.pos;
+// //   vout.screenPos = vout.pos;
 
-    vout.uv = (vin.TexCoord);
-    vout.normal = mul(vin.Normal, World);
-    vout.Diffuse = vin.Diffuse*Material.Diffuse;
-    return vout;
-}
+// //   vout.uv = (vin.TexCoord);
+// //   vout.normal = mul(vin.Normal, World);
+// //   vout.Diffuse = vin.Diffuse*Material.Diffuse;
+// //   return vout;
+//}
